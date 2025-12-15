1f165263a48cae6a8f20a6f576918318fa19224c
vacuumil <stihoplet799@yandex.ru>
2025-12-15 23:14:41 +0400

    check

---
+++ next.config.js
@@ -0,0 +1,25 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  
+  // Отключаем турбопак для сборки на Vercel
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        // Явно указываем корневую директорию
+      }
+    }
+  },
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
